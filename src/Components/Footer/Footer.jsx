2a36de21381f2cd06151810fbf6ef7f911939bc9
BBaishakhiBoral <140104874+BBaishakhiBoral@users.noreply.github.com>
2024-03-08 23:34:30 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="bg-yellow-900 text-white text-opacity-70 font-bol py-2">
       <div className="flex w-[85%] mx-auto  justify-between items-center ">
         <div></div>
-        <h1>Made by Rafikul with ❤️</h1>
+        <h1>Made by Baishakhi with ❤️</h1>
         <div className="flex  items-center space-x-5">
           <h1 className="mb-1">Follow Me</h1>
           <div className="flex space-x-3 text-white">
